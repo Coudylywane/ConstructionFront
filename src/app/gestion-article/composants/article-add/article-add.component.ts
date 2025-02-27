44308d935131115b702926956edbc1417408c6f9
seynabou27 <seynabouka09@gmail.com>
2025-02-27 21:19:27 +0000

    updateArticle

--- src/app/gestion-article/composants/article-add/article-add.component.ts
+++ src/app/gestion-article/composants/article-add/article-add.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-article-add',
   templateUrl: './article-add.component.html',
-  styleUrls: ['./article-add.component.css']
+  styleUrls: ['./article-add.component.scss']
 })
 export class ArticleAddComponent {
 
